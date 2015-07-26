efa6532117522aee06a662e2fd46aea059137408
Chris Barrett <chris@codesaru.com>
2015-07-26 15:34:44 -0700

    Fix Spritesheet blending mode.

--- src/graphics/Spritesheet.cpp
+++ src/graphics/Spritesheet.cpp
@@ -223,12 +223,12 @@ bool Spritesheet::PrepareBlendState () {
     D3D11_BLEND_DESC blendDesc;
     SecureZeroMemory(&blendDesc, sizeof(blendDesc));
     blendDesc.RenderTarget[0].BlendEnable           = TRUE;
-    blendDesc.RenderTarget[0].BlendOp               = D3D11_BLEND_OP_ADD;
     blendDesc.RenderTarget[0].SrcBlend              = D3D11_BLEND_SRC_ALPHA;
-    blendDesc.RenderTarget[0].DestBlend             = D3D11_BLEND_ONE;
-    blendDesc.RenderTarget[0].BlendOpAlpha          = D3D11_BLEND_OP_ADD;
+    blendDesc.RenderTarget[0].DestBlend             = D3D11_BLEND_INV_SRC_ALPHA;
+    blendDesc.RenderTarget[0].BlendOp               = D3D11_BLEND_OP_ADD;
     blendDesc.RenderTarget[0].SrcBlendAlpha         = D3D11_BLEND_ZERO;
     blendDesc.RenderTarget[0].DestBlendAlpha        = D3D11_BLEND_ZERO;
+    blendDesc.RenderTarget[0].BlendOpAlpha          = D3D11_BLEND_OP_ADD;
     blendDesc.RenderTarget[0].RenderTargetWriteMask = 0x0F;
 
     float blend_factor[4] = {0.0f, 0.0f, 0.0f, 0.0f};
